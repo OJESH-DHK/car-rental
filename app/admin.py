ae918c862e19ee793b1b0702fbbeeb3c3fda143d
OJESH-DHK <ojesbirajandhakal@gmail.com>
2025-06-06 16:54:29 +0545

    changes in index

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Vehicle
+from .models import Vehicle, Feature
 
 admin.site.register(Vehicle)
-
-
+admin.site.register(Feature)
--- app/models.py
+++ app/models.py
@@ -2,23 +2,32 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+class Feature(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 class Vehicle(models.Model):
     car_brand = models.CharField(max_length=100)
     car_model = models.CharField(max_length=100)
     price_per_day = models.DecimalField(max_digits=8, decimal_places=2)
+    features = models.ManyToManyField(Feature, blank=True)
     image = models.ImageField(upload_to='vehicles/')
-    mileage = models.IntegerField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    features = models.TextField(
-        help_text="Comma-separated features like: Air Conditioning, Bluetooth, GPS",
-        null=True,
-        blank=True
-    )
+    mileage = models.PositiveIntegerField( blank=True)
+    transmission = models.CharField(max_length=50 , blank=True, choices=[
+        ('manual', 'Manual'),
+        ('automatic', 'Automatic'),
+    ])
+    seats = models.PositiveIntegerField()
+    luggage_capacity = models.PositiveIntegerField(help_text="Luggage capacity in liters")
 
     def __str__(self):
         return f"{self.car_brand} {self.car_model}"
 
-    @property
-    def name(self):
-        return f"{self.car_brand} {self.car_model}"
+
+
+
